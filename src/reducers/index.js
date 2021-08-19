93ba646699cb58c56058b478653723797ace1d84
Mehdi <semlali.mehdi.1980@gmail.com>
2021-08-19 19:24:25 +0200

    add combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux"
+
 const songsReducer = () => {
     return [
         { title: 'No Scrubs', duration: '3:13' },
@@ -13,3 +15,8 @@ const SelectedSongReducer = (selectedSong = null, action) => {
     }
     return selectedSong
 }
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: SelectedSongReducer
+})
